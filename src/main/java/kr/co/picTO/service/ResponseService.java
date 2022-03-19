840395cbdf6b89ba13a9a5d54d5d82d53280b413
reidlo5135 <201840103@email.daelim.ac.kr>
2022-03-19 19:25:23 +0900

    3/19 middle commit #1

---
+++ src/main/java/kr/co/picTO/service/ResponseService.java
@@ -0,0 +1,51 @@
+package kr.co.picTO.service;
+
+import kr.co.picTO.model.response.CommonResponse;
+import kr.co.picTO.model.response.CommonResult;
+import kr.co.picTO.model.response.ListResult;
+import kr.co.picTO.model.response.SingleResult;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ResponseService {
+
+    public <T> SingleResult<T> getSingleResult(T data) {
+        SingleResult<T> result = new SingleResult<>();
+        result.setData(data);
+        setSuccessResult(result);
+        return result;
+    }
+
+    public <T> ListResult<T> getListResult(List<T> list) {
+        ListResult<T> result = new ListResult<>();
+        result.setList(list);
+        setSuccessResult(result);
+        return result;
+    }
+
+    public CommonResult getSuccessResult() {
+        CommonResult result = new CommonResult();
+        setSuccessResult(result);
+        return result;
+    }
+
+    public CommonResult getFailResult() {
+        CommonResult result = new CommonResult();
+        setFailResult(result);
+        return result;
+    }
+
+    private void setSuccessResult(CommonResult result) {
+        result.setSuccess(true);
+        result.setCode(CommonResponse.SUCCESS.getCode());
+        result.setMsg(CommonResponse.SUCCESS.getMsg());
+    }
+
+    private void setFailResult(CommonResult result) {
+        result.setSuccess(false);
+        result.setCode(CommonResponse.FAIL.getCode());
+        result.setMsg(CommonResponse.FAIL.getMsg());
+    }
+}
