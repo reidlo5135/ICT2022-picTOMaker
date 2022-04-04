04a4e7c48e2b10cea5e0c795b243a035d9fb3ac5
reidlo5135 <201840103@email.daelim.ac.kr>
2022-04-04 20:01:36 +0900

    04/04 clean

--- src/main/java/kr/co/picTO/config/security/SecurityConfiguration.java
+++ src/main/java/kr/co/picTO/config/security/SecurityConfiguration.java
@@ -1,14 +1,14 @@
 package kr.co.picTO.config.security;
 
 import kr.co.picTO.advice.exception.CustomAccessDeniedHandler;
+
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
