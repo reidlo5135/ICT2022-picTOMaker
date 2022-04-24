fc15d80a3fe245ab54704612d9c50a94e51ca920
reidlo5135 <201840103@email.daelim.ac.kr>
2022-04-24 16:30:48 +0900

    04/24 resolve Oauth google

--- src/main/java/kr/co/picTO/dto/social/ProfileDTO.java
+++ src/main/java/kr/co/picTO/dto/social/ProfileDTO.java
@@ -11,9 +11,4 @@ public class ProfileDTO {
     private String email;
     private String nickname;
     private String profile_image_url;
-
-    public ProfileDTO(String email, String nickname) {
-        this.email = email;
-        this.nickname = nickname;
-    }
 }
