76f2a9cc46966af4cbcfe4bc24b130e49a873356
reidlo5135 <201840103@email.daelim.ac.kr>
2022-04-24 16:30:48 +0900

    04/24 resolve Oauth google

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,18 +1,18 @@
 import React from "react";
 import logo from './PicTOmaker-LOGO.png';
-import {KAKAO_AUTH_URL} from "./env";
+import {KAKAO_AUTH_URL, GOOGLE_AUTH_URL, NAVER_AUTH_URL} from "./component/oauth2/env";
 import './App.css';
 
 function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
         <h1>
             <img src={logo}/>
-            <a href={KAKAO_AUTH_URL}>KAKAO LOGIN</a>
+            <a href={KAKAO_AUTH_URL}>KAKAO LOGIN</a><br></br>
+            <a href={GOOGLE_AUTH_URL}>GOOGLE LOGIN</a><br></br>
+            <a href={NAVER_AUTH_URL}>NAVER LOGIN</a>
         </h1>
-      </header>
     </div>
   );
 }
