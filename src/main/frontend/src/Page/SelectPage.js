26b767ac331990c5c805c5453da432cca2502d89
강준모 <201840103@email.daelim.ac.kr>
2022-04-29 19:06:37 +0900

    04/29 merge d -> r -> m

--- src/main/frontend/src/Page/SelectPage.js
+++ src/main/frontend/src/Page/SelectPage.js
@@ -10,28 +10,28 @@ import SelectContent from "../component/Select-Content"
 
 class SelectPage extends Component{
     constructor(props){
-      super(props);
-      this.state = {
-        Subject: {title:"Web", sub:"world wide web!"},
-        Contents: [
-          {id:1, title:"HTML", desc:"HTML is HyperText Markup Language."},
-          {id:2, title:"CSS", desc:"CSs is for design"},
-          {id:3, title:"JavaScript", desc:"JavaScript is for interactive"}
-        ]
-      }
+        super(props);
+        this.state = {
+            Subject: {title:"Web", sub:"world wide web!"},
+            Contents: [
+                {id:1, title:"HTML", desc:"HTML is HyperText Markup Language."},
+                {id:2, title:"CSS", desc:"CSs is for design"},
+                {id:3, title:"JavaScript", desc:"JavaScript is for interactive"}
+            ]
+        }
     }
     render(){
-    return (
-      <div className="App">
-        
-        <Top/>
-        <SelectContent/>
-        <Footer />
-      </div>
-    );
+        return (
+            <div className="App">
+
+                <Top/>
+                <SelectContent/>
+                <Footer />
+            </div>
+        );
     }
-  }
-  /*커밋*/
-  
-  export default SelectPage;
+}
+/*커밋*/
+
+export default SelectPage;
   
---
+++ src/main/frontend/src/component/SignUp.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import Logo from "../image/Logo.png";
+import "../css/SignUp.css"
+import "../css/font.css"
+
+class SignUp extends Component{
+    render(){
+        return (
+            <div className='SignUp'>
+                <div className='SU-Content'>
+                    <div className='SU-Logo'>
+                        <img src={Logo} alt="PictoMaker-Logo" style={{width:"278px",height:"142px"}}/>
+                    </div>
+                    <div className='SU-Input'>
+                        <form action="" method="get">
+                            <div className='SU-Form'>
+                                <div className='Label-txt'>아이디</div>
+                                <input type={'email'} placeholder="아이디"/>
+                            </div>
+                            <div className='SU-Form'>
+                                <div className='Label-txt'>비밀번호</div>
+                                <input type={'password'} placeholder="비밀번호"/>
+                            </div>
+                            <div className='SU-Form'>
+                                <div className='Label-txt'>비밀번호 확인</div>
+                                <input type={'password'} placeholder="비밀번호 확인"/>
+                            </div>
+                            <div className='SU-Form'>
+                                <div className='Label-txt'>이름</div>
+                                <input type={'text'} placeholder="이름"/>
+                            </div>
+
+                            <button className='SU-Button'>
+                                가입완료
+                            </button>
+                        </form>
+                    </div>
+                    <div className='SU-Caution'>
+                        <p>가입시에 <span>이용약관</span>과 <span>개인정보취급방침</span>에 동의한 것으로 간주합니다.</p>
+                    </div>
+
+
+                </div>
+            </div>
+        );
+    }
+}
+
+export default SignUp;
