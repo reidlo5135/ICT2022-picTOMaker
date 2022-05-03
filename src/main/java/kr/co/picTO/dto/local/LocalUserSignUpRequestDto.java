a3fac5db1d0a85f65707485a4d115c71b3c55468
강준모 <201840103@email.daelim.ac.kr>
2022-05-03 16:42:23 +0900

    05/03 temp #4

--- src/main/java/kr/co/picTO/dto/local/LocalUserSignUpRequestDto.java
+++ src/main/java/kr/co/picTO/dto/local/LocalUserSignUpRequestDto.java
@@ -4,34 +4,56 @@ import kr.co.picTO.entity.local.BaseLocalUser;
 import kr.co.picTO.entity.oauth2.BaseAuthRole;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.extern.log4j.Log4j2;
 
 import java.util.Collections;
 
 @Getter
+@Log4j2
 public class LocalUserSignUpRequestDto {
     private String email;
     private String password;
     private String name;
     private String nickName;
     private String provider;
+    private BaseAuthRole role;
 
     @Builder
-    public LocalUserSignUpRequestDto(String email, String password, String name, String nickName, String provider) {
+    public LocalUserSignUpRequestDto(String email, String password, String name, String nickName, String provider, BaseAuthRole role) {
         this.email = email;
         this.password = password;
         this.name = name;
         this.nickName = nickName;
         this.provider = provider;
+        this.role = role;
     }
 
     public BaseLocalUser toEntity() {
-        return BaseLocalUser.builder()
-                .email(email)
-                .password(password)
-                .name(name)
-                .nickName(nickName)
-                .provider(BaseAuthRole.LOCAL.toString())
-                .roles(Collections.singletonList(BaseAuthRole.LOCAL.getKey()))
-                .build();
+        if(email.equals("picTOadmin@picTOMaker.com") && email != null) {
+            String role = BaseAuthRole.ADMIN.getKey();
+            String provider = BaseAuthRole.ADMIN.toString();
+
+            log.info("Local User SRUDTO r, p : " + role + ", " + provider);
+
+            return BaseLocalUser.builder()
+                    .email(email)
+                    .password(password)
+                    .name(name)
+                    .nickName(nickName)
+                    .provider(provider)
+                    .role(BaseAuthRole.ADMIN)
+                    .roles(Collections.singletonList(role))
+                    .build();
+        } else {
+            return BaseLocalUser.builder()
+                    .email(email)
+                    .password(password)
+                    .name(name)
+                    .nickName(nickName)
+                    .provider(BaseAuthRole.LOCAL.toString())
+                    .role(BaseAuthRole.LOCAL)
+                    .roles(Collections.singletonList(BaseAuthRole.LOCAL.getKey()))
+                    .build();
+        }
     }
 }
--- src/main/java/kr/co/picTO/service/local/LocalUserService.java
+++ src/main/java/kr/co/picTO/service/local/LocalUserService.java
@@ -2,7 +2,9 @@ package kr.co.picTO.service.local;
 
 import kr.co.picTO.advice.exception.CEmailLoginFailedException;
 import kr.co.picTO.advice.exception.CUserNotFoundException;
+import kr.co.picTO.config.security.LocalUserJwtProvider;
 import kr.co.picTO.dto.local.LocalUserLoginResponseDto;
+import kr.co.picTO.dto.local.LocalUserRequestDto;
 import kr.co.picTO.dto.local.LocalUserResponseDto;
 import kr.co.picTO.dto.local.LocalUserSignUpRequestDto;
 import kr.co.picTO.entity.local.BaseLocalUser;
@@ -23,6 +25,7 @@ import java.util.stream.Collectors;
 public class LocalUserService {
     private final BaseLocalUserRepo userJpaRepo;
     private final PasswordEncoder passwordEncoder;
+    private final LocalUserJwtProvider localUserJwtProvider;
     @Transactional(readOnly = true)
     public LocalUserLoginResponseDto login(String email, String password) {
         BaseLocalUser user = userJpaRepo.findByEmail(email).orElseThrow(CEmailLoginFailedException::new);
@@ -36,6 +39,16 @@ public class LocalUserService {
         if(userJpaRepo.findByEmail(localUserSignUpRequestDto.getEmail()).orElse(null) == null)
             return userJpaRepo.save(localUserSignUpRequestDto.toEntity()).getId();
         else throw new CEmailLoginFailedException();
+    }
+    @Transactional
+    public String createToken(Long id, List<String> roles) {
+        String token = localUserJwtProvider.createToken(String.valueOf(id), roles);
+        if (token == null || token.equals(""))
+            throw new NullPointerException("Local User token is null");
+        log.info("Local User SVC i, r : " + id + ", " + roles);
+        log.info("Local User SVC createToken : " + token);
+
+        return token;
     }
     @Transactional(readOnly = true)
     public LocalUserResponseDto findById(Long id) {
@@ -55,5 +68,16 @@ public class LocalUserService {
                 .stream()
                 .map(LocalUserResponseDto::new)
                 .collect(Collectors.toList());
+    }
+    @Transactional
+    public Long update(Long id, LocalUserRequestDto userRequestDto) {
+        BaseLocalUser modifiedUser = userJpaRepo
+                .findById(id).orElseThrow(CUserNotFoundException::new);
+        modifiedUser.updateNickName(userRequestDto.getNickName());
+        return id;
+    }
+    @Transactional
+    public void delete(Long id) {
+        userJpaRepo.deleteById(id);
     }
 }
