593a2fbbac5f9678a328a7cf2360f2a7bbddaeb4
reidlo5135 <201840103@email.daelim.ac.kr>
2022-05-11 16:19:30 +0900

    05/11 alter App.js

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,6 +1,6 @@
 import React, {Component, useEffect, useState} from 'react';
 import './App.css';
-import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
+import {BrowserRouter, Route, Link, useHistory, Switch} from 'react-router-dom';
 import MainPage from './Page/MainPage';
 import SelectPage from './Page/SelectPage';
 import LoginPage from './Page/LoginPage';
@@ -10,17 +10,43 @@ import axios from "axios";
 
 class App extends Component{
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            message: ""
+        }
+    }
+
+    componentDidMount() {
+        const history = useHistory();
+        const url = 'http://localhost:8080/oauth2/login';
+        let data = {
+            email,
+            nickname,
+            profile_image_url
+        }
+        axios.get(url, JSON.stringify(data), {
+            headers: {
+                "Content-Type": `application/json`,
+            },
+        })
+            .then((res) => {
+                console.log(res);
+                history.replace("/");
+            });
+    }
+
     render(){
         return (
             <div className='App'>
                 <BrowserRouter>
-                    <Routes>
+                    <Switch>
                         <Route path = '/' element={<MainPage />}/>
                         <Route path = '/Login' element={<LoginPage />}/>
                         <Route path = '/Select' element={<SelectPage />}/>
                         <Route path = '/SignUp' element={<SignUpPage />}/>
                         <Route path = '/MyPage' element={<MyPage />}/>
-                    </Routes>
+                    </Switch>
                 </BrowserRouter>
             </div>
         );
