dca6b5be7b185074da074d4e170aff8e8e2b6902
reidlo5135 <201840103@email.daelim.ac.kr>
2022-05-12 16:50:23 +0900

    05/12 temp #3

---
+++ src/main/frontend/src/component/oauth2/callback.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+import {useEffect, useState} from 'react';
+import {BrowserRouter} from "react-router-dom";
+
+const CallbackPage = () => {
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+    })
+}
+
+export default CallbackPage;
