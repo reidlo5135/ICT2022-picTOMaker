fb22320a9b8d54f7793c53b080e52d63050be57f
reidlo5135 <201840103@email.daelim.ac.kr>
2022-05-22 15:30:23 +0900

    05/22 temp

--- src/main/frontend/src/component/MyPage/MyPage-profile.js
+++ src/main/frontend/src/component/MyPage/MyPage-profile.js
@@ -1,51 +1,63 @@
-
-import React from 'react';
-import Logo from "../../image/Logo.png";
+import React, {useEffect, useState} from 'react';
 import "../../css/MyPage.css"
 import "../../css/font.css"
-import { Link } from "react-router-dom";
 
 export default function MyPageProfile(){
+
+    const getProfile = localStorage.getItem('profile');
+
+    const [email, setEmail] = useState();
+    const [nickName, setNickName] = useState();
+    const [profileImage, setProfileImage] = useState();
+
+    const getProf = () => {
+        try {
+            const jsonProf = JSON.parse(getProfile);
+            console.log('jProf : ', jsonProf);
+
+            setEmail(jsonProf.email);
+            setNickName(jsonProf.nickname);
+            setProfileImage(jsonProf.profile_image_url);
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    useEffect(() => {
+        getProf();
+    }, []);
     
     return (
-                <div className='MyPage-Right'>
-                    <div className='Right-Contents'>
-                        <div className='Menu-Name'>
-                            내 프로필
-                        </div>
-                        <div className='MenuBox'>
-                            <div className='Name'>
-                                이름
-                            </div>
-                            <div className='MenuBox-props'>
-                                홍길동
-                            </div>
-                        </div>
-                        <div className='MenuBox'>
-                            <div className='Name'>
-                                이름
-                            </div>
-                            <div className='MenuBox-props'>
-                                홍길동
-                            </div>
-                        </div>
-                        <div className='MenuBox'>
-                            <div className='Name'>
-                                이름
-                            </div>
-                            <div className='MenuBox-props'>
-                                홍길동
-                            </div>
-                        </div>
-                        <div className='MenuBox'>
-                            <div className='Name'>
-                                이름
-                            </div>
-                            <div className='MenuBox-props'>
-                                홍길동
-                            </div>
-                        </div>
+        <div className='MyPage-Right'>
+            <div className='Right-Contents'>
+                <div className='Menu-Name'>
+                    내 프로필
+                </div>
+                <div className='MenuBox'>
+                    <div className='Name'>
+                        이름
+                    </div>
+                    <div className='MenuBox-props'>
+                        {nickName}
                     </div>
                 </div>
-    )
+                <div className='MenuBox'>
+                    <div className='Name'>
+                        이메일
+                    </div>
+                    <div className='MenuBox-props'>
+                        {email}
+                    </div>
+                </div>
+                <div className='MenuBox'>
+                    <div className='Name'>
+                        제작한 픽토
+                    </div>
+                    <div className='MenuBox-props'>
+                        0
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
