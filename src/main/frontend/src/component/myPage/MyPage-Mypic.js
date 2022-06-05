17d965b0677c16c16382c219051d489bb074e082
김동호 <ehdrh321@gmail.com>
2022-06-05 21:47:01 +0900

    06/05 21:46 ReFactor: MyPage-Mypic.js 충돌 수정

--- src/main/frontend/src/component/myPage/MyPage-Mypic.js
+++ src/main/frontend/src/component/myPage/MyPage-Mypic.js
@@ -1,4 +1,3 @@
-
 import React,{useEffect, useState} from 'react';
 import Logo from "../../image/Logo.png";
 import "../../css/MyPage.css";
@@ -7,33 +6,32 @@ import 'react-fancybox/lib/fancybox.css';
 
 const Mypic = () => {
     return(
-        <div className='mypics'>
-           <div className='pic-div'>
-               <div className='pic-cont'>
-                   <div className='pic-colorbox'/>
-                   <div className='pic-img'>
+                <div className='mypics'>
+                   <div className='pic-div'>
+                       <div className='pic-cont'>
+                           <div className='pic-colorbox'/>
+                           <div className='pic-img'>
+                           </div>
+                           <p className='pic-name'>picname</p>
+                           <div className='pic-btns'>
+                               <button className='pic-download pic-btn'>
+                                   다운로드
+                               </button>
+                               <button className='pic-edit pic-btn'>
+                                    편집하기
+                               </button>
+                           </div>
+                       </div>
                    </div>
-                   <p className='pic-name'>picname</p>
-                   <div className='pic-btns'>
-                       <button className='pic-download pic-btn'>
-                           다운로드
-                       </button>
-                       <button className='pic-edit pic-btn'>
-                            편집하기
-                       </button>
-                   </div>
-               </div>
-           </div>
-        </div>
-)
+                </div>
+    )
 }
 export default function MyPageMyPic(){
-return (
-    <div className='MyPage-Right'>
-        <div className='right-flex'>
-            <Mypic/>
-
-       </div>
-    </div>
+    return (
+            <div className='MyPage-Right'>
+                <div className='right-flex'>
+                    <Mypic/>
+               </div>
+            </div>
     )
 }
