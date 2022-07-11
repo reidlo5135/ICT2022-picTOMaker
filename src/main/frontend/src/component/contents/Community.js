7c4cd06ace14a102dd4d08b3aed36fe94a7e3d3f
김동호 <ehdrh321@gmail.com>
2022-07-11 21:51:03 +0900

    07/11 21:50 Refactor : axios , map 적용

--- src/main/frontend/src/component/contents/Community.js
+++ src/main/frontend/src/component/contents/Community.js
@@ -7,7 +7,24 @@ import Top from "./Top";
 import Footer from "./Footer";
 
 export default function Community(){
+    /* async function getData(){
+        let rawResponse = await fetch('https://jsonplaceholder.typicode.com/posts');
+        let jsonResponse = await rawResponse.json();
+        console.log(jsonResponse);
+    }
 
+    getData(); */
+        
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        axios.get('https://jsonplaceholder.typicode.com/users')
+            .then(response => {
+                setUsers(response.data);
+            });
+    }, []);
+    console.log(users)
+        
     return (
             <>
                 <Top/>
@@ -18,18 +35,28 @@ export default function Community(){
                         <table className="commu-table">
                             <thead>
                                 <tr>
-                                    <th>글 번호</th>
+                                {users.map(user => {
+                                        return (<th key={user.id}>
+                                            {user.id}
+                                        </th>)
+                                    })}
+{/*                                     <th>글 번호</th>
                                     <th>타이틀 </th>
                                     <th>작성자 </th>
-                                    <th>작성일 </th>
+                                    <th>작성일 </th> */}
                                 </tr>
                             </thead>
                             <tbody>
                                 <tr>
-                                    <td> 1 </td>
+                                {users.map(user => {
+                                        return (<td key={user.id}>
+                                            {user.name}
+                                        </td>)
+                                    })}
+{/*                                     <td> 1 </td>
                                     <td> 2 </td>
                                     <td> 3</td>
-                                    <td> 4 </td>
+                                    <td> 4 </td> */}
                                 </tr>     
                             </tbody>
                         </table>
