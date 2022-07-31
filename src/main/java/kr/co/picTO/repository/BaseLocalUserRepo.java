d893597060893f85d20310151e5a81ce08eca4c2
reidlo5135 <201840103@email.daelim.ac.kr>
2022-07-31 20:09:40 +0900

    07/31 20:09 REFACTOR: BaseLocalUserRepo.java 수정

--- src/main/java/kr/co/picTO/repository/BaseLocalUserRepo.java
+++ src/main/java/kr/co/picTO/repository/BaseLocalUserRepo.java
@@ -11,7 +11,6 @@ import java.util.Optional;
 public interface BaseLocalUserRepo extends JpaRepository<BaseLocalUser, Long> {
 
     Optional<BaseLocalUser> findByEmail(String email);
-
     Optional<BaseLocalUser> findByEmailAndProvider(String email, String provider);
 
     @Modifying
