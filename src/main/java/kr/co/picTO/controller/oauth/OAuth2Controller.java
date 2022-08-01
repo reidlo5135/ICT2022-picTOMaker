c0071011774d43f8bc005d9ec40a469dd2493ac1
reidlo5135 <201840103@email.daelim.ac.kr>
2022-08-01 12:47:07 +0900

    08/01 12:46 REFACTOR: OAuth2Controller.java logic 수정

--- src/main/java/kr/co/picTO/controller/oauth/OAuth2Controller.java
+++ src/main/java/kr/co/picTO/controller/oauth/OAuth2Controller.java
@@ -1,16 +1,10 @@
 package kr.co.picTO.controller.oauth;
 
 import io.swagger.annotations.Api;
-import kr.co.picTO.dto.social.ProfileDTO;
-import kr.co.picTO.model.response.SingleResult;
 import kr.co.picTO.service.response.ResponseLoggingService;
-import kr.co.picTO.service.response.ResponseService;
 import kr.co.picTO.service.security.OAuth2ProviderService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,7 +20,6 @@ public class OAuth2Controller {
     private static final String className = OAuth2Controller.class.toString();
 
     private final OAuth2ProviderService OAuth2ProviderService;
-    private final ResponseService responseService;
     private final ResponseLoggingService loggingService;
 
     @PostMapping(value = "/token/{provider}")
@@ -46,28 +39,12 @@ public class OAuth2Controller {
     }
 
     @PostMapping(value = "/profile/{provider}")
-    public ResponseEntity<SingleResult<ProfileDTO>> getProfile(@RequestBody Map<String, String> access_token, @PathVariable String provider) {
-        ResponseEntity<SingleResult<ProfileDTO>> ett = null;
+    public ResponseEntity<?> getProfile(@RequestBody Map<String, String> access_token, @PathVariable String provider) {
+        ResponseEntity<?> ett = null;
         loggingService.httpPathStrLoggingWithRequest(className, "getProfile", access_token.get("access_token"), provider, "");
 
         try {
-            ProfileDTO profileDTO;
-
-            if(provider.equals("google")) {
-                profileDTO = OAuth2ProviderService.getProfileForGoogle(access_token.get("access_token"), provider);
-                log.info("Prov Controller google pDTO : " + profileDTO);
-            } else {
-                profileDTO = OAuth2ProviderService.getProfile(access_token.get("access_token"), provider);
-                log.info("Prov Controller pDTO : " + profileDTO);
-            }
-
-            SingleResult result = responseService.getSingleResult(profileDTO);
-            loggingService.singleResultLogging(className, "getProfile", result);
-
-            HttpHeaders httpHeaders = new HttpHeaders();
-            httpHeaders.setContentType(MediaType.APPLICATION_JSON);
-
-            ett = new ResponseEntity<>(result, httpHeaders, HttpStatus.OK);
+            ett = OAuth2ProviderService.getProfile(access_token.get("access_token"), provider);
             log.info("Prov Controller ett : " + ett);
         }catch (Exception e) {
             e.printStackTrace();
@@ -77,20 +54,12 @@ public class OAuth2Controller {
     }
 
     @DeleteMapping(value = "/token/invalid/{access_token}")
-    public ResponseEntity<SingleResult<Long>> inValidToken(@PathVariable String access_token) {
-        ResponseEntity<SingleResult<Long>> ett = null;
+    public ResponseEntity<?> inValidToken(@PathVariable String access_token) {
+        ResponseEntity<?> ett = null;
         loggingService.httpPathStrLogging(className, "inValidToken", access_token, "", "");
 
         try {
-            Integer id = OAuth2ProviderService.deleteToken(access_token);
-
-            SingleResult result = responseService.getSingleResult(id);
-            loggingService.singleResultLogging(className, "inValidToken", result);
-
-            HttpHeaders httpHeaders = new HttpHeaders();
-            httpHeaders.setContentType(MediaType.APPLICATION_JSON);
-
-            ett = new ResponseEntity<>(result, httpHeaders, HttpStatus.OK);
+            ett = OAuth2ProviderService.deleteToken(access_token);
             log.info("Prov Controller ett : " + ett);
         } catch (Exception e) {
             e.printStackTrace();
