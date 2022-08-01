7342463342f6d82cfe6d32c9b936e34ff6869c52
reidlo5135 <201840103@email.daelim.ac.kr>
2022-08-01 12:30:26 +0900

    08/01 12:30 REFACTOR: FileUploadService.java logic 수정

--- src/main/java/kr/co/picTO/service/s3/FileUploadService.java
+++ src/main/java/kr/co/picTO/service/s3/FileUploadService.java
@@ -1,20 +1,28 @@
 package kr.co.picTO.service.s3;
 
 import com.amazonaws.services.s3.model.ObjectMetadata;
-import kr.co.picTO.advice.exception.CustomCommunicationException;
 import kr.co.picTO.advice.exception.CustomUserExistException;
 import kr.co.picTO.advice.exception.CustomUserNotFoundException;
 import kr.co.picTO.entity.local.BaseLocalUser;
 import kr.co.picTO.entity.oauth2.BaseAuthUser;
 import kr.co.picTO.entity.s3.BaseS3Image;
+import kr.co.picTO.model.response.CommonResult;
+import kr.co.picTO.model.response.ListResult;
+import kr.co.picTO.model.response.SingleResult;
 import kr.co.picTO.repository.BaseAuthUserRepo;
 import kr.co.picTO.repository.BaseLocalUserRepo;
 import kr.co.picTO.repository.BaseS3ImageRepo;
+import kr.co.picTO.service.response.ResponseLoggingService;
+import kr.co.picTO.service.response.ResponseService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.disk.DiskFileItem;
 import org.apache.commons.io.IOUtils;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
@@ -31,27 +39,32 @@ import java.util.*;
 @RequiredArgsConstructor
 public class FileUploadService {
 
+    private static final String className = FileUploadService.class.toString();
+
     private final BaseS3ImageRepo imageRepo;
     private final BaseAuthUserRepo authUserRepo;
     private final BaseLocalUserRepo localUserRepo;
     private final UploadService uploadService;
+    private final ResponseService responseService;
+    private final ResponseLoggingService loggingService;
 
     @Transactional
     public MultipartFile decodeAndConvertFile(String octet) {
-        String[] strings = octet.split(",");
-        String extension;
-        switch (strings[0]) {
-            case "data:image/jpeg;base64":
-                extension = "jpeg";
-                break;
-            case "data:image/jpg;base64":
-                extension = "jpg";
-                break;
-            default:
-                extension = "png";
-                break;
-        }
         try {
+            String[] strings = octet.split(",");
+            String extension;
+            switch (strings[0]) {
+                case "data:image/jpeg;base64":
+                    extension = "jpeg";
+                    break;
+                case "data:image/jpg;base64":
+                    extension = "jpg";
+                    break;
+                default:
+                    extension = "png";
+                    break;
+            }
+
             byte[] decoded = DatatypeConverter.parseBase64Binary(strings[1]);
             log.info("File SVC decodeAndConvertFile decoded : " + decoded);
 
@@ -94,137 +107,216 @@ public class FileUploadService {
     }
 
     @Transactional
-    public String uploadImage(MultipartFile file, String email, String provider) {
+    public ResponseEntity<?> uploadImage(MultipartFile file, String email, String provider) {
+        ResponseEntity<?> ett = null;
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+
         log.info("File SVC uploadImage email : " + email);
         log.info("File SVC uploadImage provider : " + provider);
 
-        String fileName = createFileName(file.getOriginalFilename());
-        ObjectMetadata objectMetadata = new ObjectMetadata();
-        objectMetadata.setContentLength(file.getSize());
-        objectMetadata.setContentType(file.getContentType());
-
-        log.info("File SVC uploadImg objectMetaData contentType : " + objectMetadata.getContentType());
-
-        String fileUrl = null;
-
-        try (InputStream inputStream = file.getInputStream()) {
-            log.info("File SVC uploadImage fileName : " + fileName);
-            uploadService.uploadFile(inputStream, objectMetadata, fileName);
-            fileUrl = uploadService.getFileUrl(fileName);
-
-            BaseS3Image image = null;
-            if(provider.equals("LOCAL")) {
-                BaseLocalUser blu = localUserRepo.findByEmail(email).orElseThrow();
-                image = BaseS3Image.builder()
-                       .blu(blu)
-                       .email(blu.getEmail())
-                       .fileName(fileName)
-                       .fileUrl(fileUrl)
-                       .extension(getFileExtension(fileName))
-                       .provider(provider)
-                       .build();
-                log.info("File SVC uploadImage blu : " + blu);
-                log.info("File SVC uploadImage s3 : " + image);
+        try {
+            if(file == null || file.isEmpty()) {
+                CommonResult failResult = responseService.getFailResult(-1, "uploadImage Error Occurred : File is Null");
+                loggingService.commonResultLogging(className, "uploadImage", failResult);
+                ett = new ResponseEntity<>(failResult, httpHeaders, HttpStatus.INTERNAL_SERVER_ERROR);
             } else {
-                BaseAuthUser bau = authUserRepo.findByEmail(email).orElseThrow();
-                image = BaseS3Image.builder()
-                       .bau(bau)
-                       .email(bau.getEmail())
-                       .fileName(fileName)
-                       .fileUrl(fileUrl)
-                       .extension(getFileExtension(fileName))
-                       .provider(provider)
-                       .build();
-                log.info("File SVC uploadImage bau : " + bau);
-                log.info("File SVC uploadImage s3 : " + image);
+                String fileName = createFileName(file.getOriginalFilename());
+                ObjectMetadata objectMetadata = new ObjectMetadata();
+                objectMetadata.setContentLength(file.getSize());
+                objectMetadata.setContentType(file.getContentType());
+
+                log.info("File SVC uploadImg objectMetaData contentType : " + objectMetadata.getContentType());
+
+                String fileUrl = null;
+
+                try (InputStream inputStream = file.getInputStream()) {
+                    log.info("File SVC uploadImage fileName : " + fileName);
+                    uploadService.uploadFile(inputStream, objectMetadata, fileName);
+                    fileUrl = uploadService.getFileUrl(fileName);
+
+                    BaseS3Image image = null;
+                    if(provider.equals("LOCAL")) {
+                        BaseLocalUser blu = localUserRepo.findByEmail(email).orElseThrow();
+                        image = BaseS3Image.builder()
+                                .blu(blu)
+                                .email(blu.getEmail())
+                                .fileName(fileName)
+                                .fileUrl(fileUrl)
+                                .extension(getFileExtension(fileName))
+                                .provider(provider)
+                                .build();
+                        log.info("File SVC uploadImage blu : " + blu);
+                        log.info("File SVC uploadImage s3 : " + image);
+                    } else {
+                        BaseAuthUser bau = authUserRepo.findByEmail(email).orElseThrow();
+                        image = BaseS3Image.builder()
+                                .bau(bau)
+                                .email(bau.getEmail())
+                                .fileName(fileName)
+                                .fileUrl(fileUrl)
+                                .extension(getFileExtension(fileName))
+                                .provider(provider)
+                                .build();
+                        log.info("File SVC uploadImage bau : " + bau);
+                        log.info("File SVC uploadImage s3 : " + image);
+                    }
+                    imageRepo.save(image);
+
+                    SingleResult<String> singleResult = responseService.getSingleResult(fileUrl);
+                    loggingService.singleResultLogging(className, "uploadImage", singleResult);
+                    ett = new ResponseEntity<>(singleResult, httpHeaders, HttpStatus.OK);
+                } catch (IOException e) {
+                    throw new IllegalArgumentException(String.format("File SVC uploadImage 파일 변환 중 에러가 발생했습니다 파일명 -> (%s) : ", file.getOriginalFilename()));
+                }
             }
-            imageRepo.save(image);
-        } catch (IOException e) {
-            throw new IllegalArgumentException(String.format("FILE SVC uploadImage 파일 변환 중 에러가 발생했습니다 파일명 -> (%s) : ", file.getOriginalFilename()));
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        return fileUrl;
+        log.info("File SVC uploadImage ett : " + ett);
+        return ett;
     }
 
     @Transactional
-    public Integer updatePicToByEmailAndId(MultipartFile file, String email, Long id) {
-        log.info("File SVC updatePicToByEmailAndProvider email : " + email);
-        log.info("File SVC updatePicToByEmailAndProvider id : " + id);
-        Integer result = null;
+    public ResponseEntity<?> updatePicToByEmailAndId(MultipartFile file, String email, Long id) {
+        ResponseEntity<?> ett = null;
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
 
-        String fileName = createFileName(file.getOriginalFilename());
-        ObjectMetadata objectMetadata = new ObjectMetadata();
-        objectMetadata.setContentLength(file.getSize());
-        objectMetadata.setContentType(file.getContentType());
+        log.info("File SVC updatePicToByEmailAndId email : " + email);
+        log.info("File SVC updatePicToByEmailAndId id : " + id);
+        try {
+            if(file == null || file.isEmpty()) {
+                CommonResult failResult = responseService.getFailResult(-1, "updatePicToByEmailAndId Error Occurred");
+                loggingService.commonResultLogging(className, "updatePicToByEmailAndId", failResult);
+                ett = new ResponseEntity<>(failResult, httpHeaders, HttpStatus.INTERNAL_SERVER_ERROR);
+            } else {
+                Integer result = null;
+                String fileName = createFileName(file.getOriginalFilename());
+                ObjectMetadata objectMetadata = new ObjectMetadata();
+                objectMetadata.setContentLength(file.getSize());
+                objectMetadata.setContentType(file.getContentType());
 
-        log.info("File SVC updatePicToByEmailAndProvider objectMetaData contentType : " + objectMetadata.getContentType());
+                log.info("File SVC updatePicToByEmailAndId objectMetaData contentType : " + objectMetadata.getContentType());
 
-        String fileUrl = null;
+                String fileUrl = null;
 
-        try (InputStream inputStream = file.getInputStream()) {
-            log.info("File SVC updatePicToByEmailAndProvider fileName : " + fileName);
-            uploadService.uploadFile(inputStream, objectMetadata, fileName);
-            fileUrl = uploadService.getFileUrl(fileName);
+                try (InputStream inputStream = file.getInputStream()) {
+                    log.info("File SVC updatePicToByEmailAndId fileName : " + fileName);
+                    uploadService.uploadFile(inputStream, objectMetadata, fileName);
+                    fileUrl = uploadService.getFileUrl(fileName);
 
-            result = imageRepo.updateByEmailAndId(fileUrl, email, id);
-        } catch (IOException e) {
-            throw new IllegalArgumentException(String.format("FILE SVC updatePicToByEmailAndProvider 파일 변환 중 에러가 발생했습니다 파일명 -> (%s) : ", file.getOriginalFilename()));
+                    result = imageRepo.updateByEmailAndId(fileUrl, email, id);
+
+                    SingleResult<Integer> singleResult = responseService.getSingleResult(result);
+                    loggingService.singleResultLogging(className, "updatePicToByEmailAndId", singleResult);
+                    ett = new ResponseEntity<>(singleResult, httpHeaders, HttpStatus.OK);
+                } catch (IOException e) {
+                    throw new IllegalArgumentException(String.format("File SVC updatePicToByEmailAndId 파일 변환 중 에러가 발생했습니다 파일명 -> (%s) : ", file.getOriginalFilename()));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        return result;
+        log.info("File SVC updatePicToByEmailAndId ett : " + ett);
+        return ett;
     }
 
-    public List<BaseS3Image> getPicToByEmail(String email, String provider) {
+    public ResponseEntity<?> getPicToByEmail(String email, String provider) {
+        ResponseEntity<?> ett = null;
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
         List<BaseS3Image> result = new ArrayList<>();
+
         log.info("File SVC getPicToByEmail email : " + email);
         log.info("File SVC getPicToByEmail provider : " + provider);
+
         try {
-            if(provider.equals("LOCAL")) {
-                BaseLocalUser blu = localUserRepo.findByEmailAndProvider(email, provider).orElseThrow(CustomUserExistException::new);
-                log.info("File SVC getPicToByEmail findByEmail&Prov blu : " + blu);
-                email = blu.getEmail();
+            if(email == null || provider == null) {
+                CommonResult failResult = responseService.getFailResult(-1, "getPicToByEmail Error Occurred");
+                loggingService.commonResultLogging(className, "getPicToByEmail", failResult);
+                ett = new ResponseEntity<>(failResult, httpHeaders, HttpStatus.INTERNAL_SERVER_ERROR);
             } else {
-                BaseAuthUser bau = authUserRepo.findByEmailAndProvider(email, provider).orElseThrow(CustomUserNotFoundException::new);
-                log.info("File SVC getPicToByEmail findByEmail&Prov bau : " + bau);
-                email = bau.getEmail();
-            }
-            result = imageRepo.findAllByEmail(email);
-            for(int i=0;i<result.size();i++) {
-                log.info("File SVC getPicToByEmail result List : " + Arrays.toString(result.toArray()));
+                if(provider.equals("LOCAL")) {
+                    BaseLocalUser blu = localUserRepo.findByEmailAndProvider(email, provider).orElseThrow(CustomUserExistException::new);
+                    log.info("File SVC getPicToByEmail findByEmail&Prov blu : " + blu);
+                    email = blu.getEmail();
+                } else {
+                    BaseAuthUser bau = authUserRepo.findByEmailAndProvider(email, provider).orElseThrow(CustomUserNotFoundException::new);
+                    log.info("File SVC getPicToByEmail findByEmail&Prov bau : " + bau);
+                    email = bau.getEmail();
+                }
+                result = imageRepo.findAllByEmail(email);
+                for(int i=0;i<result.size();i++) {
+                    log.info("File SVC getPicToByEmail result List : " + Arrays.toString(result.toArray()));
+                }
+                ListResult listResult = responseService.getListResult(result);
+                loggingService.listResultLogging(className, "getPicToByEmail", listResult);
+                ett = new ResponseEntity<>(listResult, httpHeaders, HttpStatus.OK);
             }
         } catch (Exception e) {
             e.printStackTrace();
             log.error("File SVC getPicTOByEmail error occurred : " + e.getMessage());
         }
-        return result;
+        log.info("File SVC getPicToByEmail ett : " + ett);
+        return ett;
     }
 
-    public Long getPicToCountByEmailAndProvider(String email, String provider) {
+    public ResponseEntity<?> getPicToCountByEmailAndProvider(String email, String provider) {
+        ResponseEntity<?> ett = null;
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+
         log.info("File SVC getPicToCountByEmailAndProvider email : " + email);
         log.info("File SVC getPicToCountByEmailAndProvider provider : " + provider);
-        try {
-            Long result = imageRepo.countByEmailAndProvider(email, provider);
-            log.info("File SVC getPicToCountByEmailAndProvider result : ", result);
 
-            return result;
+        try {
+            if(email == null || provider == null) {
+                CommonResult failResult = responseService.getFailResult(-1, "getPicToCountByEmailAndProvider Error Occurred");
+                loggingService.commonResultLogging(className, "getPicToCountByEmailAndProvider", failResult);
+                ett = new ResponseEntity<>(failResult, httpHeaders, HttpStatus.INTERNAL_SERVER_ERROR);
+            } else {
+                Long result = imageRepo.countByEmailAndProvider(email, provider);
+                log.info("File SVC getPicToCountByEmailAndProvider result : ", result);
+
+                SingleResult<Long> singleResult = responseService.getSingleResult(result);
+                loggingService.singleResultLogging(className, "getPicToCountByEmailAndProvider", singleResult);
+                ett = new ResponseEntity<>(singleResult, httpHeaders, HttpStatus.OK);
+            }
         } catch (Exception e) {
             e.printStackTrace();
             log.error("File SVC getPicToCountByEmailAndProvider error occurred : " + e.getMessage());
-            return -1L;
         }
+        log.info("File SVC getPicToByCountByEmailAndProvider ett : " + ett);
+        return ett;
     }
 
     @Transactional
-    public Integer deletePicToById(Long id) {
+    public ResponseEntity<?> deletePicToById(Long id) {
+        ResponseEntity<?> ett = null;
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+
         log.info("File SVC deletePicToById id : " + id);
-        Integer result = null;
         try {
-            result = imageRepo.deleteByFileName(id);
-            log.info("File SVC deletePicToById result : ", result);
+            if(id == null) {
+                CommonResult failResult = responseService.getFailResult(-1, "deletePicToById Error Occurred");
+                loggingService.commonResultLogging(className, "deletePicToById", failResult);
+                ett = new ResponseEntity<>(failResult, httpHeaders, HttpStatus.INTERNAL_SERVER_ERROR);
+            } else {
+                Integer result = imageRepo.deleteByFileName(id);
+                log.info("File SVC deletePicToById result : ", result);
+
+                SingleResult<Integer> singleResult = responseService.getSingleResult(result);
+                loggingService.singleResultLogging(className, "deletePicToById", singleResult);
+                ett = new ResponseEntity<>(singleResult, httpHeaders, HttpStatus.OK);
+            }
         } catch (Exception e) {
             e.printStackTrace();
             log.error("File SVC deletePicToById error occurred : " + e.getMessage());
-            throw new CustomCommunicationException();
         }
-        return result;
+        log.info("File SVC deletePicToById ett : " + ett);
+        return ett;
     }
 
     private String createFileName(String originalFileName) {
