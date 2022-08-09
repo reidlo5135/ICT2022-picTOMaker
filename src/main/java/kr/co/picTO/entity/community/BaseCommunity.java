dd915d740399bc0937b0bdafc6715e6135071e4e
강준모 <201840103@email.daelim.ac.kr>
2022-08-09 19:37:40 +0900

    08/09 19:37 FEAT: Add BaseCommunity.java

---
+++ src/main/java/kr/co/picTO/entity/community/BaseCommunity.java
@@ -0,0 +1,20 @@
+package kr.co.picTO.entity.community;
+
+import kr.co.picTO.entity.BaseTimeEntity;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "base_user_community")
+public class BaseCommunity extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+}
