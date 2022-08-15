e3c78e91c685fd75a334cfb777bb8060cf56a96a
reidlo5135 <201840103@email.daelim.ac.kr>
2022-08-15 14:34:01 +0900

    08/15 14:33 REFACTOR: modify UserCommunityRequestDto.java

--- src/main/java/kr/co/picTO/community/dto/UserCommunityRequestDto.java
+++ src/main/java/kr/co/picTO/community/dto/UserCommunityRequestDto.java
@@ -8,15 +8,37 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Log4j2
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserCommunityRequestDto {
 
+    private long id;
     private String email;
+    private String nickname;
     private String title;
     private String content;
+    private LocalDateTime createdDate;
+    private LocalDateTime modifiedDate;
+
+
+    public UserCommunityRequestDto(BaseUserCommunity baseUserCommunity) {
+        if(baseUserCommunity.getBlu() != null) {
+            email = baseUserCommunity.getBlu().getEmail();
+            nickname = baseUserCommunity.getBlu().getNickName();
+        } else if(baseUserCommunity.getBau() != null) {
+            email = baseUserCommunity.getBau().getEmail();
+            nickname = baseUserCommunity.getBau().getName();
+        }
+        id = baseUserCommunity.getId();
+        title = baseUserCommunity.getTitle();
+        content = baseUserCommunity.getContent();
+        createdDate = baseUserCommunity.getCreatedDate();
+        modifiedDate = baseUserCommunity.getModifiedDate();
+    }
 
     public BaseUserCommunity toBluEntity(BaseLocalUser blu) {
         return BaseUserCommunity.builder()
