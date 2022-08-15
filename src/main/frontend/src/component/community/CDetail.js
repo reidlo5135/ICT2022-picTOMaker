14ec91fa8729d10254b94bb72abaa89624bddac9
reidlo5135 <201840103@email.daelim.ac.kr>
2022-08-15 14:35:54 +0900

    08/15 14:34 REFACTOR: modify frontend CDetail.js

--- src/main/frontend/src/component/community/CDetail.js
+++ src/main/frontend/src/component/community/CDetail.js
@@ -3,34 +3,51 @@ import React, { useEffect, useState,useRef } from 'react';
 import Top from "../contents/Top";
 import img from "../../image/naver.png";
 
-const CDetail = ({ history, diaryList,match}) => {
-    const [data,setDate] = useState([]);
-    const dataId = useRef(0)
+const CDetail = ({ history, diaryList, match}) => {
+    const [data,setData] = useState([]);
+    const dataId = useRef(0);
+    console.log('CDetail matchParams : ' + match.params.id);
 
+    // const getData = async () => {
+    //     const res = await fetch(
+    //         "https://jsonplaceholder.typicode.com/comments"
+    //     ).then((res) => res.json());
+    //
+    //     const initData = res.slice(0, 20).map((it) => {
+    //         //slice = 0부터 20까지 데이터를 자를것임
+    //         return {
+    //             author: it.email,
+    //             content: it.body,
+    //             emotion: Math.floor(Math.random() * 5) + 1,
+    //             //Math.random()*5 = 0부터 4까지의 난수 생성(소수점까지 포함)
+    //             //Math.floor = 소수점을 없애줌 , +1 = 5까지
+    //             created_date: new Date().getTime() + 1,
+    //             id: dataId.current++
+    //         };
+    //     });
+    //     setData(initData[`${match.params.id}`]);
+    // };
 
     const getData = async () => {
-        const res = await fetch(
-            "https://jsonplaceholder.typicode.com/comments"
-        ).then((res) => res.json());
+        await axios.get(`/v1/api/community/find/${match.params.id}`)
+            .then((response) => {
+                console.log('response : ', response.data);
+                console.log('response : ', response.data.data);
 
-        const initData = res.slice(0, 20).map((it) => {
-            //slice = 0부터 20까지 데이터를 자를것임
-            return {
-                author: it.email,
-                content: it.body,
-                emotion: Math.floor(Math.random() * 5) + 1,
-                //Math.random()*5 = 0부터 4까지의 난수 생성(소수점까지 포함)
-                //Math.floor = 소수점을 없애줌 , +1 = 5까지
-                created_date: new Date().getTime() + 1,
-                id: dataId.current++
-            };
-        });
-        setDate(initData[`${match.params.id}`]);
-    };
+                if(response.data.code === 0) {
+                    const res = response.data.data;
+                    console.log('community detail res : ' + res);
+                    setData(res);
+                }
+            })
+            .catch((err) => {
+                console.error('err : ', JSON.stringify(err));
+                alert(err.response.data.msg);
+            });
+    }
 
     useEffect(() => {
         getData();
-
     }, []);
     console.log(data);
 
@@ -50,15 +67,15 @@ const CDetail = ({ history, diaryList,match}) => {
                                 <div className='postcont'>
                                     <div className="post-view-row row1">
                                         <label className='profname plabel'>작성자</label>
-                                        <p>{ data.author }</p>
+                                        <p>{ data.nickname }</p>
                                     </div>
                                     <div className="post-view-row row2">
                                         <label className='proftitle plabel'>제목</label>
-                                        <p>{ data.id }</p>
+                                        <p>{ data.title }</p>
                                     </div>
                                     <div className="post-view-row row3">
                                         <label className='profdate plabel'>작성일</label>
-                                        <p>{ data.created_date }</p>
+                                        <p>{ data.createdDate }</p>
                                     </div>
                                 </div>
                             </div>
