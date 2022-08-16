7cb8f2f5b21b214e592ded2dbfe6ca2c02cfd7fc
강준모 <201840103@email.daelim.ac.kr>
2022-08-16 14:56:55 +0900

    08/16 14:55 FEAT: modify frontend Community.js

--- src/main/frontend/src/component/community/Community.js
+++ src/main/frontend/src/component/community/Community.js
@@ -14,26 +14,6 @@ function Community() {
 
   const dataId = useRef(0);
 
-  // const getData = async () => {
-  //   const res = await fetch(
-  //     "https://jsonplaceholder.typicode.com/comments"
-  //   ).then((res) => res.json());
-  //
-  //   const initData = res.slice(0, 20).map((it) => {
-  //     //slice = 0부터 20까지 데이터를 자를것임
-  //     return {
-  //       author: it.email,
-  //       content: it.body,
-  //       emotion: Math.floor(Math.random() * 5) + 1,
-  //       //Math.random()*5 = 0부터 4까지의 난수 생성(소수점까지 포함)
-  //       //Math.floor = 소수점을 없애줌 , +1 = 5까지
-  //       create_date: new Date().getTime() + 1,
-  //       id: dataId.current++
-  //     };
-  //   });
-  //   setDate(initData);
-  // };
-
   const getData = async () => {
     try {
       await axios.get('/v1/api/community/find/all')
