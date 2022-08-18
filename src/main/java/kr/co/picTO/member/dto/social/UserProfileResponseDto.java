2afc77574e0a5b9e5c2a8161555f4c9523d4002a
강준모 <201840103@email.daelim.ac.kr>
2022-08-18 13:02:41 +0900

    08/18 13:02 REFACTOR: Rename UserProfileDto.java
    -> UserProfileResponseDto.java

--- src/main/java/kr/co/picTO/member/dto/social/UserProfileResponseDto.java
+++ src/main/java/kr/co/picTO/member/dto/social/UserProfileResponseDto.java
@@ -5,19 +5,19 @@ import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
-public class UserProfileDto {
+public class UserProfileResponseDto {
     private String email;
     private String name;
     private String nickname;
     private String profile_image_url;
 
-    public UserProfileDto(String email, String nickname, String profile_image_url) {
+    public UserProfileResponseDto(String email, String nickname, String profile_image_url) {
         this.email = email;
         this.nickname = nickname;
         this.profile_image_url = profile_image_url;
     }
 
-    public UserProfileDto(String email, String name, String nickname, String profile_image_url) {
+    public UserProfileResponseDto(String email, String name, String nickname, String profile_image_url) {
         this.email = email;
         this.name = name;
         this.nickname = nickname;
