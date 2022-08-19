697791e55bb498f33a05b8c4567f4baebca08b12
김동호 <ehdrh321@gmail.com>
2022-08-19 14:21:50 +0900

    08/19 14:21 Add : 댓글 컴포넌트 추가(미완)

---
+++ src/main/frontend/src/component/community/Ccmt.js
@@ -0,0 +1,67 @@
+import {useRef, useState} from "react";
+import "../css/Diary.css";
+
+const Ccmt = ({onCreate}) => {
+    const authorInput = useRef();
+    const contentInput = useRef();
+
+    const [state, setState] = useState({
+        author:"",
+        content:"",
+        emotion:1,
+    });
+
+    const handleChangeDiary = (e) => {
+        setState({
+            ...state,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    const handleSubmit = () => {
+        if (state.author.length < 1) {
+          authorInput.current.focus();
+          return;
+        }
+    
+        if (state.content.length < 5) {
+          contentInput.current.focus();
+          return;
+        }
+    
+        onCreate(state.author, state.content, state.emotion);
+        alert("저장 성공");
+        setState({
+          author: "",
+          content: "",
+          emotion: 1
+        });
+      };
+    return(
+        <div className="Ccmt">
+            <div>
+                <input
+                ref={authorInput}
+                name="author"
+                type="text"
+                value={state.author}
+                onChange={handleChangeDiary}
+                />
+                <div>
+                    <textarea
+                    ref={contentInput}
+                    name="content"
+                    type="text"
+                    value={state.content}
+                    onChange={handleChangeDiary}
+                    />
+                </div>
+            </div>
+            <div>
+                <button onClick={handleSubmit}>저장하기</button>
+            </div>
+        </div>
+    )
+}
+
+export default Ccmt;
