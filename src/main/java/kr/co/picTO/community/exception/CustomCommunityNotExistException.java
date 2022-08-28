653ac155dfa4719bf333ee1691e3549867719a25
reidlo5135 <201840103@email.daelim.ac.kr>
2022-08-28 16:25:33 +0900

    08/28 16:25 FEAT: Add CustomCommunityNotExistException.java

---
+++ src/main/java/kr/co/picTO/community/exception/CustomCommunityNotExistException.java
@@ -0,0 +1,15 @@
+package kr.co.picTO.community.exception;
+
+public class CustomCommunityNotExistException extends RuntimeException{
+    public CustomCommunityNotExistException() {
+        super();
+    }
+
+    public CustomCommunityNotExistException(String message) {
+        super(message);
+    }
+
+    public CustomCommunityNotExistException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
