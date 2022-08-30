7615ae05833dec45257c3ecfc1aa4f2750b5cd68
reidlo5135 <201840103@email.daelim.ac.kr>
2022-08-30 10:43:36 +0900

    08/30 10:43 REFACTOR: Remove @Log4j2

--- src/main/java/kr/co/picTO/community/application/UserCommunityService.java
+++ src/main/java/kr/co/picTO/community/application/UserCommunityService.java
@@ -14,13 +14,11 @@ import kr.co.picTO.user.domain.social.BaseAuthUser;
 import kr.co.picTO.user.domain.social.BaseAuthUserRepo;
 import kr.co.picTO.user.exception.CustomUserNotFoundException;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Log4j2
 @Service
 @RequiredArgsConstructor
 public class UserCommunityService {
