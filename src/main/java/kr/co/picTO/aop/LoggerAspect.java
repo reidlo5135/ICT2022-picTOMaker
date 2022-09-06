c34e674590ece9bc284fd65ed217ee3185663607
reidlo5135 <201840103@email.daelim.ac.kr>
2022-09-06 10:55:53 +0900

    REFACTOR: Move Directory LoggerAspect.java

--- src/main/java/kr/co/picTO/aop/LoggerAspect.java
+++ src/main/java/kr/co/picTO/aop/LoggerAspect.java
@@ -1,4 +1,4 @@
-package kr.co.picTO.common.aop;
+package kr.co.picTO.aop;
 
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -34,22 +34,6 @@ public class LoggerAspect {
         log.info(">> Repository Request : {}.{}({})", pjp.getTarget().getClass().getInterfaces()[0].getName(), pjp.getSignature().getName(), pjp.getArgs());
         Object result = pjp.proceed();
         log.info(">> Repository Result : {}", result);
-        return result;
-    }
-
-    @Around(value = "execution(public * kr.co.picTO.admin.presentation.*.*Controller.*(..))")
-    public Object logAdminControllers(ProceedingJoinPoint pjp) throws Throwable {
-        log.info(">> Admin Controller Request : {}.{}({})", pjp.getTarget().getClass().getName(), pjp.getSignature().getName(), pjp.getArgs());
-        Object result = pjp.proceed();
-        log.info(">> Admin Controller Result : {}", result);
-        return result;
-    }
-
-    @Around(value = "execution(public * kr.co.picTO.admin.application.*.*Service.*(..))")
-    public Object logAdminServices(ProceedingJoinPoint pjp) throws Throwable {
-        log.info(">> Admin Service Request : {}.{}({})", pjp.getTarget().getClass().getName(), pjp.getSignature().getName(), pjp.getArgs());
-        Object result = pjp.proceed();
-        log.info(">> Admin Service Result : {}", result);
         return result;
     }
 }
