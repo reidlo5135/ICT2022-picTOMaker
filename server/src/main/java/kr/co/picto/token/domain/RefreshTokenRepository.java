8f12c942656de9c471933239a52030e4dd64e507
강준모 <201840103@email.daelim.ac.kr>
2022-09-19 16:50:05 +0900

    Refactor: Add RefreshToken Update(renewlogin)

--- server/src/main/java/kr/co/picto/token/domain/RefreshTokenRepository.java
+++ server/src/main/java/kr/co/picto/token/domain/RefreshTokenRepository.java
@@ -1,6 +1,8 @@
 package kr.co.picto.token.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
@@ -8,5 +10,8 @@ public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long
 
     Optional<RefreshToken> findByTokenId(Long tokenId);
 
+    @Modifying
+    @Query(value = "UPDATE RefreshToken rt SET rt.token = :token WHERE rt.tokenId = :id")
+    Integer updateById(String token, Long id);
     void deleteByTokenId(Long tokenId);
 }
