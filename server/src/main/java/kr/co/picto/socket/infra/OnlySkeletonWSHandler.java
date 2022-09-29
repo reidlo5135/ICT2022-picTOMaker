dd6bcef70d43ba1e91f8086dd585cb72df8f6476
강준모 <201840103@email.daelim.ac.kr>
2022-09-29 19:18:51 +0900

    Refactor: Convert to TextWebSockethandler

--- server/src/main/java/kr/co/picto/socket/infra/OnlySkeletonWSHandler.java
+++ server/src/main/java/kr/co/picto/socket/infra/OnlySkeletonWSHandler.java
@@ -1,47 +1,67 @@
 package kr.co.picto.socket.infra;
 
 import lombok.extern.log4j.Log4j2;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
 
-import javax.websocket.*;
-import javax.websocket.server.ServerEndpoint;
-import java.io.IOException;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 @Log4j2
 @Component
-@ServerEndpoint(value = "/socket/picto")
-public class OnlySkeletonWSHandler {
-    private static Set<Session> clients = Collections.synchronizedSet(new HashSet<Session>());
-    private static Map<String, String> data = new HashMap<>();
+public class OnlySkeletonWSHandler extends TextWebSocketHandler {
+    HashMap<String, WebSocketSession> sessionMap = new HashMap<>();
+    private static Map<String, JSONObject> data = new HashMap<>();
 
-    @OnOpen
-    public void onOpen(Session session) {
-        if(!clients.contains(session)) {
-            clients.add(session);
-            log.info("WebSocket sessionId : " + session.getId());
-            log.info("WebSocket sessionRemote : " + session.getBasicRemote());
-        } else{
-            log.info("WebSocket Session is Already Connected");
-        }
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        super.afterConnectionEstablished(session);
+        sessionMap.put(session.getId(), session);
     }
 
-    @OnMessage
-    public void onMessage(String message, Session session) throws IOException {
-        log.info("WebSocket onMessage session : " + session.getId());
-        log.info("WebSocket send message : " + message);
-        for (Session s : clients) {
-            if(message.equals("editTool")) {
-                s.getBasicRemote().sendText(data.get("skeleton"));
-            } else {
-                data.put("skeleton", message);
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) {
+        String msg = message.getPayload();
+        log.info("payload : " + msg);
+        JSONObject obj = jsonToObjectParser(msg);
+        log.info("JSONMap : " + obj);
+        for(String key : sessionMap.keySet()) {
+            WebSocketSession wss = sessionMap.get(key);
+            try {
+                if(msg.equals("editTool")) {
+                    log.info("EditTool DataMap : " + data);
+                    wss.sendMessage(new TextMessage(data.get("toBrowser").toJSONString()));
+                } else {
+                    data.put("toBrowser", obj);
+                }
+                wss.sendMessage(new TextMessage(obj.toJSONString()));
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         }
     }
 
-    @OnClose
-    public void onClose(Session session) {
-        clients.remove(session);
-        data.clear();
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        sessionMap.remove(session.getId());
+        super.afterConnectionClosed(session, status);
     }
+    private static JSONObject jsonToObjectParser(String jsonStr) {
+        log.info("JSON STR : " + jsonStr);
+        JSONParser parser = new JSONParser();
+        JSONObject obj = null;
+        try {
+            obj = (JSONObject) parser.parse(jsonStr);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return obj;
+    }
+
 }
