2ff86d71743b2da55bd691e826ef7c43cee57ea7
reidlo5135 <201840103@email.daelim.ac.kr>
2022-10-15 19:18:53 +0900

    Feat: Add User Classes

--- server-socket/src/main/java/kr/co/picto/user/domain/User.java
+++ server-socket/src/main/java/kr/co/picto/user/domain/User.java
@@ -1,7 +1,6 @@
 package kr.co.picto.user.domain;
 
 import kr.co.picto.common.domain.BaseTimeEntity;
-import kr.co.picto.user.TypeEnum;
 import lombok.*;
 
 import javax.persistence.*;
@@ -21,22 +20,6 @@ public class User extends BaseTimeEntity {
     @Column(nullable = false, unique = true, length = 30)
     private String email;
 
-    @Column(nullable = false, length = 100)
+    @Column(nullable = false, length = 30)
     private String provider;
-
-    @Column(nullable = false, length = 100)
-    private String skeleton;
-
-    @Column(nullable = false, length = 100)
-    private int thick;
-
-    @Column(nullable = false, length = 100)
-    private String lineColor;
-
-    @Column(nullable = false, length = 100)
-    private String backgroundColor;
-
-    @Enumerated(EnumType.STRING)
-    @Column(name = "type", nullable = false)
-    private TypeEnum type;
 }
