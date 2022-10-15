de460d111f6944b6b68964de7074035404e19cd9
reidlo5135 <201840103@email.daelim.ac.kr>
2022-10-15 11:09:52 +0900

    Feat: socket-server initial commit

---
+++ server-socket/src/main/java/kr/co/picto/SocketServer.java
@@ -0,0 +1,13 @@
+package kr.co.picto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocketServer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocketServer.class, args);
+	}
+
+}
