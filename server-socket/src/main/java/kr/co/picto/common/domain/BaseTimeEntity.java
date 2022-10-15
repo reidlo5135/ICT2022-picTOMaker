d2e257dc8aa8a129dc82f11a105b819640b5676e
reidlo5135 <201840103@email.daelim.ac.kr>
2022-10-15 12:14:26 +0900

    Feat: Add BaseTimeEntity.java

---
+++ server-socket/src/main/java/kr/co/picto/common/domain/BaseTimeEntity.java
@@ -0,0 +1,30 @@
+package kr.co.picto.common.domain;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity implements Serializable {
+    private static final long SerialVersionUID = 9141498463876264960L;
+
+    @CreatedDate
+    @Column(updatable = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    @Column(updatable = true)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+    private LocalDateTime modifiedDate;
+}
