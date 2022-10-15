d67fb8b1f2558e680438d6ff6a2350c053628cc7
reidlo5135 <201840103@email.daelim.ac.kr>
2022-10-15 19:16:58 +0900

    Refactor: Modify Common Configurations' Classes

--- server-socket/src/main/java/kr/co/picto/SocketServer.java
+++ server-socket/src/main/java/kr/co/picto/SocketServer.java
@@ -2,12 +2,24 @@ package kr.co.picto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class SocketServer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SocketServer.class, args);
 	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+	@Bean
+	public HttpHeaders httpHeaders() {return new HttpHeaders();}
 
 }
